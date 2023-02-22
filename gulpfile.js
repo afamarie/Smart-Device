7b59a97346bd98b73dc9026d0160a224f516c402
afamaria <marie.afanasyeva@gmail.com>
2023-02-22 21:14:05 +0300

    Added html, scss files, fonts, images, responsive header stylization

--- gulpfile.js
+++ gulpfile.js
@@ -39,8 +39,9 @@ const refresh = (done) => {
   done();
 };
 
-const build = gulp.series(clean, copy, sprite, gulp.parallel(styles, js, optimizePng, optimizeJpg, optimizeSvg));
-const dev = gulp.series(clean, copy, sprite, gulp.parallel(styles, js, optimizePng, optimizeJpg, optimizeSvg), syncServer);
+//deleted optimizeJpg optimizePng
+const build = gulp.series(clean, copy, sprite, gulp.parallel(styles, js, optimizeSvg));
+const dev = gulp.series(clean, copy, sprite, gulp.parallel(styles, js, optimizeSvg), syncServer);
 const start = gulp.series(clean, copy, sprite, gulp.parallel(styles, js), syncServer);
 
 export { createWebp as webp, build, start, dev};
